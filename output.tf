11ec8bc1602ee493021710cd2f7c31ca9d6196b5
Kevin Edwards <kedwards@kevinedwards.ca>
2019-12-26 20:07:53 -0700

    removed asg and alb, only simple ec2 deploy

--- output.tf
+++ output.tf
@@ -1,20 +1,19 @@
 output "public_dns" {
   description = "The public DNS adress for the instance"
-  value       = concat(aws_instance.this.*.public_dns, [""])[0]
-
+  value       = aws_instance.this.*.public_dns
 }
 
 output "public_ip" {
   description = "The public IP address of the instance"
-  value       = concat(aws_instance.this.*.public_ip, [""])[0]
+  value       = aws_instance.this.*.public_ip
 }
 
 output "private_dns" {
   description = "The private DNS adress for the instance"
-  value       = concat(aws_instance.this.*.private_dns, [""])[0]
+  value       = aws_instance.this.*.private_dns
 }
 
 output "private_ip" {
   description = "The private IP address of the instance"
-  value       = concat(aws_instance.this.*.private_ip, [""])[0]
+  value       = aws_instance.this.*.private_ip
 }
--- vars.tf
+++ vars.tf
@@ -6,19 +6,13 @@ variable "ami" {
   type        = string
 }
 
-variable "launch_name_prefix" {
-  description = "The launch config name-prefix, Terraform will add a random string at the end to keep it unique."
-  type        = string
-}
-
-variable "protocol" {
-  default     = "HTTP"
-  description = "Protocol used to communicate with target group."
-  type        = string
-}
-
 variable "provisioning_key" {
   description = "A key that can be used to connect and provision instances in AWS."
+  type        = string
+}
+
+variable "subnet" {
+  description = "The subnet to place the instance."
   type        = string
 }
 
@@ -27,41 +21,12 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "vpc_zone_identifier" {
-  description = "The subnets to place the instance."
-  type        = list(string)
-}
-
 #
 # optional
 #
-variable "alb_security_groups" {
-  default     = []
-  description = "The list of security groups to attach with the load balancer."
-  type        = list(string)
-}
-
-variable "alb_tags" {
-  default     = {}
-  description = "Additional tags for the alb"
-  type        = map(string)
-}
-
 variable "associate_public_ip_address" {
   default     = true
   description = "Associate a public IP addrss on launch."
-  type        = bool
-}
-
-variable "create_instance" {
-  default     = true
-  description = "Create an ec2 instance with no ASG, or ALB."
-  type        = bool
-}
-
-variable "create_alb" {
-  default     = false
-  description = "Whether to create an ALB."
   type        = bool
 }
 
@@ -71,22 +36,10 @@ variable "create_key_pair" {
   type        = bool
 }
 
-variable "desired_capacity" {
-  default     = 1
-  description = "The desired number of instances to run."
-  type        = number
-}
-
 variable "enable_monitoring" {
   default     = false
-  description = "Enable monitoring of the instance."
+  description = "Enable detailed monitoring of the instance."
   type        = bool
-}
-
-variable "health_check_type" {
-  default     = "EC2"
-  description = "The type of health check to configure. 'EC2' or 'ELB'"
-  type        = string
 }
 
 variable "instance_tags" {
@@ -107,39 +60,9 @@ variable "instance_type" {
   type        = string
 }
 
-variable "internal_lb" {
-  default     = false
-  description = "Whether to create an internal load balancer."
-  type        = bool
-}
-
-variable "load_balancer_type" {
-  default     = "application"
-  description = "Type of load balancer"
-  type        = string
-}
-
-variable "max_size" {
-  default     = 1
-  description = "The maximum number of instances."
-  type        = number
-}
-
-variable "min_size" {
-  default     = 0
-  description = "The minimum number of instances."
-  type        = number
-}
-
 variable "name" {
   default     = "instance"
   description = "The name of the instance."
-  type        = string
-}
-
-variable "path" {
-  default     = "/"
-  description = "The alb health check path."
   type        = string
 }
 
@@ -155,22 +78,10 @@ variable "security_groups" {
   type        = list(string)
 }
 
-variable "subnets" {
-  default     = []
-  description = "The subnets use for the ALB."
-  type        = list(string)
-}
-
 variable "tags" {
-  default     = [{}]
+  default     = {}
   description = "Additional tags for the instance"
-  type        = list(map(string))
-}
-
-variable "target_group_port" {
-  default     = "80"
-  description = "Alb target port."
-  type        = string
+  type        = map(string)
 }
 
 variable "user_data" {
